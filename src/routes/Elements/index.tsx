ff6879cb4d52a8b5a7f6b6a2244d15ac03b609e2
Sixtus Innocent <sixtus@Sixtuss-MacBook-Pro.local>
2024-02-02 16:47:13 +0100

    Fetch elements integration

--- src/routes/Elements/index.tsx
+++ src/routes/Elements/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ToolBar } from './partials/ToolBar';
-import { EmptyContent } from '../../components/shareds/EmptyContent';
+import { ElementsTableWithAsyncState } from './partials/ElementsTable';
 
 const Elements = () => (
-  <div style={{ minHeight: '612px' }}>
-    <h2>Elements</h2>
-    <ToolBar />
-    <div style={{ height: '100%' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <EmptyContent message="There are no elements to display" />
-      </div>
+  <div className="Elements">
+    <h1 className="main-heading mb-30">Elements</h1>
+    <div className="mb-30">
+      <ToolBar />
+    </div>
+    <div>
+      <ElementsTableWithAsyncState />
     </div>
   </div>
 );
--- src/store/slices/element.slice.ts
+++ src/store/slices/element.slice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { SerializedError, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import request, { ERequestStatus } from '../../common/request';
 import type { RootState } from '..';
 
@@ -26,16 +26,20 @@ export type Element = {
 
 export type ElementState = {
   elements: Element[];
+  total: number;
   status: ERequestStatus;
+  error: SerializedError | null;
 };
 
 const initialState: ElementState = {
   elements: [],
+  total: 0,
   status: ERequestStatus.IDLE,
+  error: null,
 };
 
 export const fetchElements = createAsyncThunk('element/fetchElements', async () => {
-  const response = await request.get<Element[]>('elements');
+  const response = await request.get<{ data: { content: Element[]; total: number } }>('elements');
   return response;
 });
 
@@ -44,14 +48,28 @@ export const elementSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchElements.pending, (_state) => {
-      const state = _state;
-      state.status = ERequestStatus.LOADING;
-    });
+    builder
+      .addCase(fetchElements.pending, (_state) => {
+        const state = _state;
+        state.status = ERequestStatus.LOADING;
+      })
+      .addCase(fetchElements.fulfilled, (_state, action) => {
+        const state = _state;
+        state.status = ERequestStatus.SUCCEEDED;
+        state.elements = action.payload.data.content;
+        state.total = action.payload.data.total;
+        state.error = null;
+      })
+      .addCase(fetchElements.rejected, (_state, action) => {
+        const state = _state;
+        state.error = action.error;
+        state.status = ERequestStatus.FAILED;
+      });
   },
 });
 
-export const selectElements = (state: RootState) => state.elements.elements;
-export const selectStatus = (state: RootState) => state.elements.status;
+export const selectElementsState = ({
+  elements: { status, elements, error, total },
+}: RootState) => ({ elements, status, error, total });
 
 export default elementSlice.reducer;
